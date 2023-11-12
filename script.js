f318dbf42cefd5ff298b5edf422a67e3a594bb4a
muhamad refaldo <49105762+muhamad-refaldo@users.noreply.github.com>
2023-11-13 02:13:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let nama = ('muhamad refaldo') //tipe string
+let usia = ('19') // tipe integer number
+let tinggiBadan = (170) //tipe double float
+let beratBadan = (65)
+let pacar = ('tidak punya')
+
+alert(`halo semua nama saya ${nama} usia saya ${usia} dan saya ${pacar} pacar HAHAHAHA`);
+nama = prompt('Siapa Nama Kamu')
+alert('Nama Kamu Adalah ' + nama)
+alert("selamat melihat linktree yang ga seberapa ini");
